1eb77e8d5f7aba01d3503bbfb7998d9e37b3c5da
YHW <ymfsder@YHWdeMacBook-Pro.local>
2022-01-19 15:42:53 +0800

    require.context 优化路由的加载

--- src/routes/modules/UserRouter.js
+++ src/routes/modules/UserRouter.js
@@ -1,9 +1,10 @@
 import Router from 'koa-router'
-import UserController from '../api/UserController'
+import UserController from '../../api/UserController'
 
 const router = new Router()
 router.prefix('/user')
 
 router.get('/sign', UserController.userSign)
+router.post('/updateUserInfo', UserController.updateUserInfo)
 
 export default router
